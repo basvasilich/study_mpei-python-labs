243b59f507625051a63ac83ef625fe9d0cab5cc8
Vadim Patsev <vadim@smekalka.com>
2019-12-01 21:13:12 +0300

    lab1

--- lab1_module.py
+++ lab1_module.py
@@ -1,14 +1,16 @@
-import math;
+import math
+from typing import Callable
 
-def max_of_vector(vector):
+
+def max_of_vector(vector: list) -> float:
     return max(vector)
 
 
-def min_of_vector(vector):
+def min_of_vector(vector: list) -> float:
     return min(vector)
 
 
-def sum_positive_vector(vector):
+def sum_positive_vector(vector: list) -> float:
     result = 0
     for item in vector:
         if item > 0:
@@ -16,21 +18,39 @@ def sum_positive_vector(vector):
     return result
 
 
-def count_positive_vector(vector):
+def count_positive_vector(vector: list) -> int:
     result = 0
     for item in vector:
         if item > 0:
             result += 1
-    return result;
+    return result
 
 
-def lab1_task1(vector_a, vector_b, vector_c):
+def lab1_task1(vector_a: list, vector_b: list, vector_c: list) -> float:
     return (max_of_vector(vector_a) + min_of_vector(vector_b)) * 2.3 + max_of_vector(vector_c)
 
 
-def func1(x):
+def func1(x: float):
     return math.sin(math.pow(x, 2)) + 1
 
 
-def func2(x):
+def func2(x: float):
     return math.pow(x, 3) + 2.5
+
+
+def make_row(var: float, f1: Callable[[float], str], **keyword_parameters: Callable[[float], str]) -> str:
+    if 'f2' in keyword_parameters:
+        return f"{str(var):6.4}{str(f1(var)):6.4}{str(keyword_parameters['f2'](var)):6.4}\n"
+    return f"{str(var):6.4}{str(f1(var)):6.4}\n"
+
+
+def make_header(name1: str, **keyword_parameters: str) -> str:
+    if 'name2' in keyword_parameters:
+        return f"""
+ X     {name1}    {keyword_parameters['name2']}
+-------------------
+"""
+    return f"""
+ X     {name1}
+-----------
+"""
--- lab1_task1.py
+++ lab1_task1.py
@@ -1,23 +1,13 @@
 from lab1_module import *
 
-vector_a = [];
-vector_b = [];
-vector_c = [];
-vector_a_str = input('Input Vector A (1,2,3): ');
-vector_b_str = input('Input Vector B (1,2,3): ');
-vector_c_str = input('Input Vector C (1,2,3): ');
+vectors = {'A': [], 'B': [], 'C': []}
 
-for i in vector_a_str[0::2]:
-    vector_a.append(float(i));
+for vector in vectors:
+    vector_str = input('Input Vector {0} (1,2,3): '.format(vector))
+    vectors[vector] = list(map(lambda i: int(i), vector_str.split(',')))
 
-for i in vector_b_str[0::2]:
-    vector_b.append(float(i));
+result_str = "Result: " + str(lab1_task1(vectors['A'], vectors['B'], vectors['C']))
 
-for i in vector_c_str[0::2]:
-    vector_c.append(float(i));
-
-result_str = "Result: " + str(lab1_task1(vector_a, vector_b, vector_c));
-
-f = open('output.txt', 'w');
-f.write(result_str);
-print(result_str);
+f = open('output.txt', 'w')
+f.write(result_str)
+print(result_str)
